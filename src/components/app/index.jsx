861fd97d41228141135ca3f6f0523bac3c8644e5
SuperCoolNinja <sbpro570@gmail.com>
2024-07-31 22:50:15 +0200

    can now save it as PDF, added some responsive for print

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -4,13 +4,26 @@ import { DataProvider } from "../../contexts/dataContext";
 import { Experiences } from "../experiences";
 import { Card } from "../shared/card";
 import { Resume } from "../resume";
+import { Button } from "../shared/button";
 
 export const App = () => {
+  const handleExportPDF = () => {
+    window.print();
+  };
+
   return (
     <DataProvider>
       <div className="container app_wrapper">
         {/* Left side, cards options */}
         <div className="cards-wrapper">
+          <Card title="Save to PDF">
+            <Button
+              label={"Save"}
+              handle={handleExportPDF}
+              style={"btn-save"}
+            />
+          </Card>
+
           <Card title="Personal Details">
             <PersonalDetailsCard />
           </Card>
